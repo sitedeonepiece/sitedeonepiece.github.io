531634e689cadb9962c4b38c0ef96499ef304169
sitedeonepiece <117922730+sitedeonepiece@users.noreply.github.com>
2022-11-11 14:23:08 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,10 @@
+function umClique(){
+  document.getElementById("logo").src="One-Piece.png";
+  
+}
+
+
+function doisCliques(){
+  document.getElementById("logo").src="logo de one piece.png";
+  
+}
